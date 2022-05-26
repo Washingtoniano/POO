820cf23f00b560749784d91becc6ba401bb4e640
Alfredo Cordero <103980015+Washingtoniano@users.noreply.github.com>
2022-05-26 17:37:03 -0300

    Add files via upload

---
+++ Unidad 3/Ejercicio 5/interfaz.py
@@ -0,0 +1,8 @@
+from zope.interface import Interface
+class interfaz (Interface):
+	def insertar_elemento(pos, objeto):
+		pass
+	def agregar_elemento(objeto):
+		pass
+	def mostrar_elemento(pos):
+		pass 
